ea3e90c4463d5d588c25150fca0cbee47ae3ba30
Joao Lucas <129006408+joaolucasrossato@users.noreply.github.com>
2024-05-15 10:12:32 -0300

    Seguradora.java

--- src/Seguradora/Seguradora.java
+++ src/Seguradora/Seguradora.java
@@ -16,55 +16,55 @@ public class Seguradora {
     }
 
     public void inserirSeguro(Seguro seguro) {
-        Seguro[] novoArray = new Seguro[seguros.length + 1];
-        for (int i = 0; i < seguros.length; i++) {
-            novoArray[i] = seguros[i];
-        }
-        novoArray[novoArray.length - 1] = seguro;
-        seguros = novoArray;
+    	for(int i =0; i<seguros.length;i++) {
+    		if(seguros[i] == null) {
+    			seguros[i] = seguro;
+    			return;
+    		}
+    	}
     }
 
-    public boolean removerSeguro(int numeroApolice) {
-        for (int i = 0; i < seguros.length; i++) {
-            if (seguros[i].getNumeroApolice() == numeroApolice) {
-                for (int j = i; j < seguros.length - 1; j++) {
-                    seguros[j] = seguros[j + 1];
-                }
-                Seguro[] novoArray = new Seguro[seguros.length - 1];
-                for (int k = 0; k < novoArray.length; k++) {
-                    novoArray[k] = seguros[k];
-                }
-                seguros = novoArray;
-                return true;
-            }
-        }
-        return false;
-    }
+   public boolean removerSeguro(int numeroApolice) {
+	   if (seguros != null) {
+		   for(int i = 0; i < seguros.length; i++) {
+			   if(seguros[i].getNumeroApolice() == numeroApolice) {
+				   seguros[i] = null;
+				   return true;
+			   }
+		   }
+	   }
+	   return false;
 
-    public boolean alteraSeguro(int numeroApolice, Seguro novoSeguro) {
-        for (int i = 0; i < seguros.length; i++) {
-            if (seguros[i].getNumeroApolice() == numeroApolice) {
-                seguros[i] = novoSeguro;
-                return true;
-            }
-        }
-        return false;
+   }
+
+    public boolean alteraSeguro(int numeroApolice, Seguro seguro) {
+    	if(seguros!=null) {
+    		for(int i=0; i < seguros.length; i++) {
+    			if(seguros[i].getNumeroApolice() == numeroApolice) {
+    				seguros[i] = seguro;
+    				return true;
+    			}
+    		}
+    	}
+    	return false;
     }
 
     public void mostraTodosSeguros() {
-        for (Seguro seguro : seguros) {
-            seguro.mostraSeguro();
-            System.out.println();
+        if (seguros != null){
+            for (int i = 0; i < seguros.length; i++) {
+                System.out.println(seguros[i].toString());
+            }
         }
     }
 
     public Seguro buscarSeguro(int numeroApolice) {
-        for (Seguro seguro : seguros) {
-            if (seguro.getNumeroApolice() == numeroApolice) {
-                return seguro;
-            }
-        }
-        return null;
+    	if(seguros != null) {
+    		for(int i = 0; i < seguros.length; i++) {
+    			if(seguros[i].getNumeroApolice()==numeroApolice) {
+    				return seguros[i];
+    			}
+    		}
+    	}
+    	return null;
     }
 }
-
