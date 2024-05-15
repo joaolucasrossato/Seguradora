aadc56c1bdd1fcd84221f4ecab565e97c27e0753
Joao Lucas <129006408+joaolucasrossato@users.noreply.github.com>
2024-05-15 10:11:29 -0300

    Data.java

--- src/Seguradora/Data.java
+++ src/Seguradora/Data.java
@@ -1,79 +1,71 @@
-// Implementar o metodo construtor e o  metodo get para cada um dos atributos.
-// O método construtor deve chamar o metodo setData.
-// O méto setData deve aceitar apenas datas válidas.
-
-// setData( dia: int, mes : int, ano : int) void
-// getData(): String
-// toString(): String
-
 package Seguradora;
 
 public class Data {
-    private int dia;
-    private int mes;
-    private int ano;
+	private int dia;
+	private int mês;
+	private int ano;
 
     public Data(int dia, int mes, int ano) {
         setData(dia, mes, ano);
     }
-
-    public void setData(int dia, int mes, int ano) {
-        if (dia > 0 && dia <= 31 && mes > 0 && mes <= 12 && ano > 0) {
-            this.dia = dia;
-            this.mes = mes;
-            this.ano = ano;
-        } else {
-            throw new IllegalArgumentException("Data inválida");
-        }
-    }
-
-    public int getDia() {
-        return dia;
-    }
-
-    public void setDia(int dia) {
-        if (dia > 0 && dia <= 31) {
-            this.dia = dia;
-        } else {
-            throw new IllegalArgumentException("Dia inválido");
-        }
-    }
-
-    public int getMes() {
-        return mes;
-    }
-
-    public void setMes(int mes) {
-        if (mes > 0 && mes <= 12) {
-            this.mes = mes;
-        } else {
-            throw new IllegalArgumentException("Mês inválido");
-        }
-    }
-
-    public int getAno() {
-        return ano;
-    }
-
-    public void setAno(int ano) {
-        if (ano > 0) {
-            this.ano = ano;
-        } else {
-            throw new IllegalArgumentException("Ano inválido");
-        }
-    }
-
+	
+	public void seData(int dia, int mês, int ano) {
+		if(dia > 0 && dia <= 31 && mês > 0 && mês <=12 && ano > 0) {
+			this.dia = dia;
+			this.mês = mês;
+			this.ano = ano;
+		}
+		else {
+			System.out.println("Data inválida");
+		}
+	}
     public String getData() {
         return dia + "/" + mes + "/" + ano;
     }
 
-    @Override
-    public String toString() {
-        return "Data{" +
-                "dia=" + dia +
-                ", mes=" + mes +
-                ", ano=" + ano +
-                '}';
-    }
-}
+	public int getDia() {
+		return dia;
+	}
 
+	public void setDia(int dia) {
+		if (dia > 0 && dia <= 31) {
+			this.dia = dia;
+		}
+		else {
+			System.out.println("Dta inválida");
+		}
+	}
+
+	public int getMês() {
+		return mês;
+	}
+
+	public void setMês(int mês) {
+		if (mês > 0 && mês <=12) {
+			this.mês = mês;
+		}
+		else {
+			System.out.println("Data inválida");
+		}
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		if (ano > 0) {
+			this.ano = ano;
+		}
+		else {
+			System.out.println("Data inválida");
+		}
+	}
+	
+	public String toString() {
+		return "Dia/" + dia
+				+"Mês/" + mês
+				+"Ano"+ano;
+	}
+	
+}
